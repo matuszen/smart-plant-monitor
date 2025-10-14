3a9a98bf7d7cb7dee3b365fc6f99c579dcbe4972
Mateusz Nowak <mateuszn641@gmail.com>
2025-10-14 17:56:39 +0200

    Update CMake configuration for pico 2 w and add lwIP options header file

---
+++ include/lwipopts.h
@@ -0,0 +1,59 @@
+#ifndef _LWIPOPTS_H
+#define _LWIPOPTS_H
+
+// General lwIP options
+#define NO_SYS 1
+#define LWIP_SOCKET 0
+#define LWIP_NETCONN 0
+
+// DHCP options
+#define LWIP_DHCP 1
+#define LWIP_DHCP_CHECK_LINK_UP 1
+
+// ARP options
+#define LWIP_ARP 1
+#define LWIP_ETHERNET 1
+
+// IP options
+#define LWIP_IPV4 1
+#define LWIP_ICMP 1
+
+// TCP options
+#define LWIP_TCP 1
+#define TCP_MSS 1460
+#define TCP_SND_BUF (8 * TCP_MSS)
+#define TCP_WND (8 * TCP_MSS)
+
+// UDP options
+#define LWIP_UDP 1
+
+// DNS options
+#define LWIP_DNS 1
+#define DNS_MAX_SERVERS 2
+
+// IGMP options (for multicast)
+#define LWIP_IGMP 1
+
+// Network interface options
+#define LWIP_NETIF_HOSTNAME 1
+#define LWIP_NETIF_STATUS_CALLBACK 1
+#define LWIP_NETIF_LINK_CALLBACK 1
+
+// Stats options
+#define LWIP_STATS 0
+#define LWIP_STATS_DISPLAY 0
+
+// Checksum options
+#define LWIP_CHECKSUM_ON_COPY 1
+
+// Thread options (not used with NO_SYS)
+#define TCPIP_THREAD_STACKSIZE 1024
+#define DEFAULT_THREAD_STACKSIZE 512
+
+// Memory options
+#define MEM_ALIGNMENT 4
+#define MEMP_NUM_TCP_SEG 32
+#define MEMP_NUM_PBUF 24
+#define PBUF_POOL_SIZE 24
+
+#endif /* _LWIPOPTS_H */
